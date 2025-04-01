03eeff42d1e6cd0c7610d31e409df37b1a1333e6
DirsanA <dirsanantehun739@gmail.com>
2025-04-01 01:53:27 -0700

    Added:Landing page is added

---
+++ my-app/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>
+        <div className="bg-black text-white">hello</div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
