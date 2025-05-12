ee6eac88d258150420dc472a6d5bb9c2c3a5837d
DirsanA <dirsanantehun739@gmail.com>
2025-05-12 03:08:50 -0700

    confing updated

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1227",
+  password: "",
   database: "digital_app",
 });
 
