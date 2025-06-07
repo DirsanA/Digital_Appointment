0238aa1e79a94e54358b43dd43c14f0d367ed72f
DirsanA <dirsanantehun739@gmail.com>
2025-06-07 02:12:52 -0700

    fix appointment page logout routing

--- client/my-app/src/Appointments.jsx
+++ client/my-app/src/Appointments.jsx
@@ -163,7 +163,7 @@ const Appointments = () => {
           </nav>
         </div>
         <Link
-          to="/logout"
+          to="/"
           className="flex items-center space-x-2 hover:bg-red-50 p-2 rounded-md text-red-500"
           onClick={() => setSidebarOpen(false)}
         >
